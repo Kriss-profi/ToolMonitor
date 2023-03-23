8275c9f7ee0616fe7d4e16f36a9669e483071e0c
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-03-23 13:35:14 +0100

    New Generic Repository

---
+++ ToolMonitor.DataAccess/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ToolMonitor.DataAccess
+{
+    using ToolMonitor.DataAccess.Entities;
+    public interface IRepository<T> where T : EntityBase
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
