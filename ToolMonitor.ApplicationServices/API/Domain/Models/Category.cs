328106e00fb0907097631e7f276bd2c2add8945a
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-07-10 23:05:17 +0200

    Added new Entities: Category, Dealer, Department, Employee, Manufacturer and User.

---
+++ ToolMonitor.ApplicationServices/API/Domain/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolMonitor.ApplicationServices.API.Domain.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ ToolMonitor.ApplicationServices/API/Domain/Models/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolMonitor.ApplicationServices.API.Domain.Models
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Web { get; set; }
+    }
+}
