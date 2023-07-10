7a8cdd5af4c8cc3cb1040b5776012850ae362972
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-07-10 23:07:30 +0200

    Added new Request and Response for Category.

---
+++ ToolMonitor.ApplicationServices/API/Domain/Categorys/GetCategoryResponse.cs
@@ -0,0 +1,8 @@
+﻿using ToolMonitor.DataAccess.Entities;
+
+namespace ToolMonitor.ApplicationServices.API.Domain.Categorys
+{
+    public class GetCategoryResponse : ResponseBase<List<Category>>
+    {
+    }
+}
