1234ce6a1b987531e22725a2765d70e0e21c73a3
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-09-01 00:28:51 +0200

    First Validator

---
+++ ToolMonitor.ApplicationServices/API/Validators/AddCompanyRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolMonitor.ApplicationServices.API.Domain.Companies;
+
+namespace ToolMonitor.ApplicationServices.API.Validators
+{
+    public class AddCompanyRequestValidator : AbstractValidator<AddCompanyRequest>
+    {
+        public AddCompanyRequestValidator()
+        {
+            this.RuleFor(x => x.CompanyName).Length(1, 100);
+            this.RuleFor(x => x.CompanyCity).Length(1, 50);
+        }
+    }
+}
