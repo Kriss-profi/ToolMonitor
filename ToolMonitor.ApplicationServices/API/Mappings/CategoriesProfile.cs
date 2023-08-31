02908192689bd2853be0aefc0df5e60e9bd045cb
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-09-01 00:26:08 +0200

    Modified CategoriesProfile and ToolsProfile

--- ToolMonitor.ApplicationServices/API/Mappings/CategoriesProfile.cs
+++ ToolMonitor.ApplicationServices/API/Mappings/CategoriesProfile.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToolMonitor.ApplicationServices.API.Domain.Categories;
 using ToolMonitor.ApplicationServices.API.Domain.Models;
 
 namespace ToolMonitor.ApplicationServices.API.Mappings
@@ -14,14 +15,24 @@ namespace ToolMonitor.ApplicationServices.API.Mappings
         {
             this.CreateMap<DataAccess.Entities.Category, Category>()
                 .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
-                //.ForMember(x => x.CompanyId, y => y.MapFrom(z => z.CompanyId))
-                .ForMember(x => x.CategoryName, y => y.MapFrom(z => z.CategoryName));
-
+                .ForMember(x => x.CategoryName, y => y.MapFrom(z => z.CategoryName))
+                ;
 
             this.CreateMap<Category, DataAccess.Entities.Category>()
                 .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
                 //.ForMember(x => x.CompanyId, y => y.MapFrom(z => z.CompanyId))
-                .ForMember(x => x.CategoryName, y => y.MapFrom(z => z.CategoryName));
+                .ForMember(x => x.CategoryName, y => y.MapFrom(z => z.CategoryName))
+                ;
+
+            this.CreateMap<AddCategoryRequest, DataAccess.Entities.Category>()
+                .ForMember(x => x.CategoryName, y => y.MapFrom(z => z.CategoryName))
+                ;
+
+            this.CreateMap<PutCategoryRequest, DataAccess.Entities.Category>()
+                .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
+                //.ForMember(x => x.CompanyId, y => y.MapFrom(z => z.CompanyId))
+                .ForMember(x => x.CategoryName, y => y.MapFrom(z => z.CategoryName))
+                ;
         }
     }
 }
