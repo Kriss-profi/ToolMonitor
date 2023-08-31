f90ff3c25c766e15141e0a6cfb42d1d75fe27884
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-08-31 18:33:03 +0200

    Categories Added  Add, Put, und Delete Requests Und Responses

---
+++ ToolMonitor.ApplicationServices/API/Domain/Categories/AddCategoryRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolMonitor.ApplicationServices.API.Domain.Categories
+{
+    public class AddCategoryRequest : IRequest<AddCategoryResponse>
+    {
+        //public int CompanyId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
