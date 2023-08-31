f27ef1c51f1ac0b1f11fa935df2c07bf0952554d
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-08-31 18:45:18 +0200

    New Migration

---
+++ ToolMonitor.DataAccess/Migrations/20230831164241_SmallChangesInTools.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToolMonitor.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SmallChangesInTools : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Web",
+                table: "Dealers",
+                newName: "DealerWeb");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Dealers",
+                newName: "DealerName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DealerWeb",
+                table: "Dealers",
+                newName: "Web");
+
+            migrationBuilder.RenameColumn(
+                name: "DealerName",
+                table: "Dealers",
+                newName: "Name");
+        }
+    }
+}
