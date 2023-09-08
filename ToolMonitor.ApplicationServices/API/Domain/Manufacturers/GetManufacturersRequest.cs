a0719bbf1d280159ebf302ff6bdba0544a5352e9
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-09-08 18:58:58 +0200

    Added two constructors in Manufactures Request

--- ToolMonitor.ApplicationServices/API/Domain/Manufacturers/GetManufacturersRequest.cs
+++ ToolMonitor.ApplicationServices/API/Domain/Manufacturers/GetManufacturersRequest.cs
@@ -9,5 +9,14 @@ namespace ToolMonitor.ApplicationServices.API.Domain.Manufacturers
 {
     public class GetManufacturersRequest : IRequest<GetManufacturersResponse>
     {
+        public string Manufacturer { get; set; }
+        public GetManufacturersRequest()
+        {
+            Manufacturer = "";
+        }
+        public GetManufacturersRequest(string manufacturerName)
+        {
+            Manufacturer = manufacturerName;
+        }
     }
 }
