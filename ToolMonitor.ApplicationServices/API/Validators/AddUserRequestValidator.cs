60cdf88a6eaae4e9c86434c788e7be0957064fae
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-09-11 02:11:55 +0200

    Added Validetor for AddUser

---
+++ ToolMonitor.ApplicationServices/API/Validators/AddUserRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolMonitor.ApplicationServices.API.Domain.Users;
+
+namespace ToolMonitor.ApplicationServices.API.Validators
+{
+    public class AddUserRequestValidator : AbstractValidator<AddUserRequest>
+    {
+        public AddUserRequestValidator()
+        {
+            this.RuleFor(x => x.FirstName).Length(1, 50);
+            this.RuleFor(x => x.LastName).Length(1, 50);
+            this.RuleFor(x => x.Email).Length(1, 50);
+            RuleFor(p => p.Password).NotEmpty().WithMessage("Your password cannot be empty")
+                    .MinimumLength(8).WithMessage("Your password length must be at least 8.")
+                    .MaximumLength(16).WithMessage("Your password length must not exceed 16.")
+                    .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
+                    .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
+                    .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
+                    .Matches(@"[\!\?\*\.]+").WithMessage("Your password must contain at least one (!? *.).");
+
+        }
+    }
+}
