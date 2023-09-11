21cde795048932ddee557503d260503b183a1b5a
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-09-11 02:18:02 +0200

    Added new Files

---
+++ ToolMonitor.ApplicationServices/API/Handlers/DeleteUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolMonitor.ApplicationServices.API.Domain.Users;
+using ToolMonitor.DataAccess.CQRS;
+using ToolMonitor.DataAccess.CQRS.Commands;
+
+namespace ToolMonitor.ApplicationServices.API.Handlers
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserRequest, DeleteUserResponse>
+    {
+        private readonly ICommandExecutor commandExecutor;
+
+        public DeleteUserHandler(ICommandExecutor commandExecutor)
+        {
+            this.commandExecutor = commandExecutor;
+        }
+        public async Task<DeleteUserResponse> Handle(DeleteUserRequest request, CancellationToken cancellationToken)
+        {
+            var query = new DeleteUserCommand() { Id = request.Id };
+            var response = await commandExecutor.Execute(query);
+            return new DeleteUserResponse();
+        }
+    }
+}
---
+++ ToolMonitor.ApplicationServices/API/Handlers/PutUserHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolMonitor.ApplicationServices.API.Domain.Users;
+using ToolMonitor.DataAccess.CQRS;
+using ToolMonitor.DataAccess.CQRS.Commands;
+using ToolMonitor.DataAccess.Entities;
+
+namespace ToolMonitor.ApplicationServices.API.Handlers
+{
+    public class PutUserHandler : IRequestHandler<PutUserRequest, PutUserResponse>
+    {
+        private readonly ICommandExecutor commandExecutor;
+        private readonly IMapper mapper;
+
+        public PutUserHandler(ICommandExecutor commandExecutor, IMapper mapper)
+        {
+            this.commandExecutor = commandExecutor;
+            this.mapper = mapper;
+        }
+        public async Task<PutUserResponse> Handle(PutUserRequest request, CancellationToken cancellationToken)
+        {
+            var user = this.mapper.Map<User>(request);
+            var command = new PutUserCommand() { Parameter = user };
+            var userFromDb = await commandExecutor.Execute(command);
+            var response = new PutUserResponse()
+            {
+                Data = this.mapper.Map<Domain.Models.User>(userFromDb),
+            };
+            return response;
+        }
+    }
+}
