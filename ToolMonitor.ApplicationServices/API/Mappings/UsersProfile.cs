d456c98e8d7690bebaa248898c69cf63ac09537f
Krzysztof Jakubowski <jakubowski@polprofi.de>
2023-09-11 02:11:12 +0200

    Added new Profile for user

--- ToolMonitor.ApplicationServices/API/Mappings/UsersProfile.cs
+++ ToolMonitor.ApplicationServices/API/Mappings/UsersProfile.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToolMonitor.ApplicationServices.API.Domain.Models;
+using ToolMonitor.ApplicationServices.API.Domain.Users;
 
 namespace ToolMonitor.ApplicationServices.API.Mappings
 {
@@ -19,6 +20,33 @@ namespace ToolMonitor.ApplicationServices.API.Mappings
                 .ForMember(x => x.LastName, y => y.MapFrom(z => z.LastName))
                 .ForMember(x => x.Email, y => y.MapFrom(z => z.Email))
                 //.ForMember(x => x.UserStatus, y => y.MapFrom(z => z.UserStatus))
+                ;
+
+            this.CreateMap<User, DataAccess.Entities.User>()
+                .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
+                .ForMember(x => x.CompanyId, y => y.MapFrom(z => z.CompanyId))
+                .ForMember(x => x.FirstName, y => y.MapFrom(z => z.FirstName))
+                .ForMember(x => x.LastName, y => y.MapFrom(z => z.LastName))
+                .ForMember(x => x.Email, y => y.MapFrom(z => z.Email))
+                //.ForMember(x => x.UserStatus, y => y.MapFrom(z => z.UserStatus))
+                ;
+
+            this.CreateMap<AddUserRequest, DataAccess.Entities.User>()
+                .ForMember(x => x.CompanyId, y => y.MapFrom(z => z.CompanyId))
+                .ForMember(x => x.FirstName, y => y.MapFrom(z => z.FirstName))
+                .ForMember(x => x.LastName, y => y.MapFrom(z => z.LastName))
+                .ForMember(x => x.Email, y => y.MapFrom(z => z.Email))
+                .ForMember(x => x.UserStatus, y => y.MapFrom(z => z.UserStatus))
+                ;
+
+            this.CreateMap<PutUserRequest, DataAccess.Entities.User>()
+                .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
+                .ForMember(x => x.CompanyId, y => y.MapFrom(z => z.CompanyId))
+                .ForMember(x => x.FirstName, y => y.MapFrom(z => z.FirstName))
+                .ForMember(x => x.LastName, y => y.MapFrom(z => z.LastName))
+                .ForMember(x => x.Email, y => y.MapFrom(z => z.Email))
+                .ForMember(x => x.Password, y => y.MapFrom(z => z.Password))
+                .ForMember(x => x.UserStatus, y => y.MapFrom(z => z.UserStatus))
                 ;
         }
     }
